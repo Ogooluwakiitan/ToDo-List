7b0e9bfba0437193c2ba23376065f3c5d3485e3b
Ogooluwakiitan <ogooluwaolaimeji@gmail.com>
2023-03-17 10:28:38 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// create simple todo list that has the following functionalities
+
+/**
+ * add a new item to the todo list
+ * edit an item in the todo list
+ * remove an item in the todo list
+ * 
+ * - the input field must be reset whwn the add button is clicked on
+ * the todo must show the most recent first
+ */
+
+let todoList = [];
+
+function addItem = () {
+
+}
+
+function editItem = (item) {
+
+}
+
+function deleteItem = (item) {
+
+}
